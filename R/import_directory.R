89da0041f5a317b19f3de3e97a86dbb10216719f
Julian Rees <julianrees@users.noreply.github.com>
2017-02-27 14:39:00 -0800

    fixed directory translation into import command

--- R/import_directory.R
+++ R/import_directory.R
@@ -2,7 +2,7 @@ import_directory <- function(directory, fileextension, separator, header) {
   # this function imports all files of a specified filetype (extension) into the workspace
   files = dir(directory)
   for(f in files){
-    strg <- paste('/Users/jarees/Projects/LLNL_UzcS_binding/Data/Luminescence/JRA01501/', f, sep = "")
+    strg <- paste(directory, f, sep = "")
     dname = strsplit(f, fileextension)[1]
     a <- paste("assign('",as.character(dname),
                "', read.csv(strg, sep ='", separator, "', header =",
