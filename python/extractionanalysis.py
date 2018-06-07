7484f97e516e9034f82c7791eba99e7c75565786
kshield <kshield@berkeley.edu>
2018-06-06 22:41:14 -0700

    Attempted to start making analysis fx to be called inside the loop

--- python/extractionanalysis.py
+++ python/extractionanalysis.py
@@ -34,124 +34,127 @@ if howmanyfiles == '1':
     print('quit now')
     filename = 'ExperTable.csv'
     print(filename)
-    my_data = datasplit = pd.read_csv(filename)
-    print(datasplit.ix[:,0])
-# If there is more than one file, first use splitLSC.py to split properly.
+    analysis()
+
+# If there is more than one file, first use splitLSC.sh to split properly.
 # Everything else is a loop through all the files.
 else:
     for value in range(int(howmanyfiles)):
+        # Filenames are in this format when they come out of splitLSC.sh
         filename = str(value+1)+'.ExperTable.csv'
         print (filename)
-        # Filenames are in this format when
-        my_data = datasplit = pd.read_csv(filename)
-
-        # Prints the datatable's first column (sample numbers) for referencing sample info
-        print(datasplit.ix[:,0])
-        
-        # The exported data is always called 'ExperTable.csv'
-
-        # Making the Table for csv Export
-        datamerge = pd.DataFrame()
+        analysis()
+        print(value)
 
 
-        # Separate the data into aqueous and organic tables
-        datasplit_aqueous = datasplit[datasplit['S#']%2 ==0]
-        datasplit_organic = datasplit[datasplit['S#']%2!=0]
+def analysis():
+    # Imports the data
+    my_data = datasplit = pd.read_csv(filename)
 
-        datasplit_aqueous.reset_index(drop=True, inplace=True)
-        datasplit_organic.reset_index(drop=True, inplace=True)
+    # Prints the datatable's first column (sample numbers) for referencing sample info
+    print(datasplit.ix[:,0])
 
-         # The aq. concentrations will need to be inputted at the start of the program
+    # Making the table to get filled with data and export
+    datamerge = pd.DataFrame()
 
-        aqueous_concentrations = input('Please input the aqeous concentrations (mM): ')
-        aqueous_concentrations = [int(x) for x in aqueous_concentrations.split(',')]
-        aqueous_concentrations = pd.Series(aqueous_concentrations).values
-        print (aqueous_concentrations)
+    # Separate the data into aqueous and organic tables
+    datasplit_aqueous = datasplit[datasplit['S#']%2 ==0]
+    datasplit_organic = datasplit[datasplit['S#']%2!=0]
 
+    datasplit_aqueous.reset_index(drop=True, inplace=True)
+    datasplit_organic.reset_index(drop=True, inplace=True)
 
-        # Add other information to the appropriate tables (to make future data analysis super easy) including:
-        # # - the isotope
-        # # - the aq. ligand & concentration
-        # # - the org. ligand & concentration
-        #
-         #isotope
-        isotope = input('What isotope?')
-        datasplit_aqueous.loc[:,'Isotope'] = isotope
-        print(datasplit_aqueous)
-        #
-        #ligands
-        aqueous_ligand = input('What was the aqueous ligand?')
-        organic_ligand = input('What was the organic ligand?')
-        datasplit_aqueous.loc[:,'Aq_Ligand'] = aqueous_ligand
-        datasplit_organic.loc[:,'Org_Ligand'] = organic_ligand
-        #
-        #Concentrations
-        organic_concentration = input('What was the organic concentration (M)?')
-        datasplit_organic.loc[:,'Org_Conc (M)'] = organic_concentration
-        #
-        datasplit_aqueous['Aq_Conc (mM)'] = aqueous_concentrations
-        #
-        print(datasplit_aqueous)
-        print(datasplit_organic)
+     # The aq. concentrations will need to be inputted at the start of the program
 
-        datamerge["AqCPMA"] = datasplit_aqueous.CPMA
-        datamerge["OrgCPMA"] = datasplit_organic.CPMA
-        #
-        # change the CPM values to adjust to 100 uL volumes (CPMA = CPM/100uL)
-        # datasplit_aqueous.loc[:,'CPMA'] /= 1
-        datasplit_organic.loc[:,'CPMA'] /= 2
-        #
-
-        print(datamerge)
-        #
-        datasplit_aqueous = datasplit_aqueous.rename({'CPMA':'CPM'},axis='columns')
-        datasplit_organic = datasplit_organic.rename({'CPMA':'CPM'},axis='columns')
+    aqueous_concentrations = input('Please input the aqeous concentrations (mM): ')
+    aqueous_concentrations = [int(x) for x in aqueous_concentrations.split(',')]
+    aqueous_concentrations = pd.Series(aqueous_concentrations).values
+    print (aqueous_concentrations)
 
 
-        # Create two new variables as lists from the CPM values
-        organicvalues = datasplit_organic["CPM"].values
-        aqueousvalues = datasplit_aqueous["CPM"].values
-        # organicvalues.size
+    # Add other information to the appropriate tables (to make future data analysis super easy) including:
+    # # - the isotope
+    # # - the aq. ligand & concentration
+    # # - the org. ligand & concentration
+    #
+     #isotope
+    isotope = input('What isotope?')
+    datasplit_aqueous.loc[:,'Isotope'] = isotope
+    print(datasplit_aqueous)
+    #
+    #ligands
+    aqueous_ligand = input('What was the aqueous ligand?')
+    organic_ligand = input('What was the organic ligand?')
+    datasplit_aqueous.loc[:,'Aq_Ligand'] = aqueous_ligand
+    datasplit_organic.loc[:,'Org_Ligand'] = organic_ligand
+    #
+    #Concentrations
+    organic_concentration = input('What was the organic concentration (M)?')
+    datasplit_organic.loc[:,'Org_Conc (M)'] = organic_concentration
+    #
+    datasplit_aqueous['Aq_Conc (mM)'] = aqueous_concentrations
+    #
+    print(datasplit_aqueous)
+    print(datasplit_organic)
 
-        datamerge["AqCPM"] = datasplit_aqueous.CPM
-        datamerge["OrgCPM"] = datasplit_organic.CPM
+    datamerge["AqCPMA"] = datasplit_aqueous.CPMA
+    datamerge["OrgCPMA"] = datasplit_organic.CPMA
+    #
+    # change the CPM values to adjust to 100 uL volumes (CPMA = CPM/100uL)
+    # datasplit_aqueous.loc[:,'CPMA'] /= 1
+    datasplit_organic.loc[:,'CPMA'] /= 2
+    #
 
-        #
-        # Solve for the % extraction
-        # (% ext. = org. CPM/(org. CPM + aq CPM))
+    print(datamerge)
+    #
+    datasplit_aqueous = datasplit_aqueous.rename({'CPMA':'CPM'},axis='columns')
+    datasplit_organic = datasplit_organic.rename({'CPMA':'CPM'},axis='columns')
 
-        extractionpercent=[]
-        for line in range(0,organicvalues.size):
-            extractionpercent.append(organicvalues[line]/(aqueousvalues[line]+organicvalues[line]))
-                #datasplit_organic.loc[organic,"CPMA"]/(datasplit_aqueous.loc[aqueous,"CPMA"]+datasplit_organic.loc[organic,"CPMA"])
-        extractionpercent
 
-        datamerge['Ext%'] = extractionpercent
-        datamerge['AqS#'] = datasplit_aqueous['S#']
-        datamerge['OrgS#'] = datasplit_organic['S#']
-        datamerge['Count Time'] = datasplit_organic['Count Time']
-        datamerge['DATE'] = datasplit_aqueous.DATE
-        datamerge['Isotope'] = datasplit_aqueous.Isotope
-        datamerge['AqL'] = datasplit_aqueous.Aq_Ligand
-        datamerge['AqLConc (mM)'] = datasplit_aqueous['Aq_Conc (mM)']
-        datamerge['OrgL'] = datasplit_organic.Org_Ligand
-        datamerge['OrgLConc (M)'] = datasplit_organic['Org_Conc (M)']
-        #
-        #
-        # # ### Time to plot this
-        #
-        date = datetime.datetime.strptime(datamerge.loc[0,'DATE'],'%m/%d/%Y').strftime('%Y%m%d')
-        #
-        print(datamerge)
-        #
+    # Create two new variables as lists from the CPM values
+    organicvalues = datasplit_organic["CPM"].values
+    aqueousvalues = datasplit_aqueous["CPM"].values
+    # organicvalues.size
 
-        # plt.plot(aqueous_concentrations,extractionpercent,'bo')
-        # plt.xscale('log')
-        # plt.title('HDEHP Extracts Gd Down to 2.5 mM')
-        # plt.xlabel('HDEHP Concentration (M)')
-        # plt.ylabel('% Extraction')
-        # plt.grid(True)
-        # plt.savefig(date+isotope+'_'+aqueous_ligand+'_'+organic_ligand+'.png')
-        # plt.show()
-        #export the data file as a CSV
-        datamerge.to_csv(date+isotope+'_'+aqueous_ligand+'_'+organic_ligand+'.csv')
+    datamerge["AqCPM"] = datasplit_aqueous.CPM
+    datamerge["OrgCPM"] = datasplit_organic.CPM
+
+    #
+    # Solve for the % extraction
+    # (% ext. = org. CPM/(org. CPM + aq CPM))
+
+    extractionpercent=[]
+    for line in range(0,organicvalues.size):
+        extractionpercent.append(organicvalues[line]/(aqueousvalues[line]+organicvalues[line]))
+            #datasplit_organic.loc[organic,"CPMA"]/(datasplit_aqueous.loc[aqueous,"CPMA"]+datasplit_organic.loc[organic,"CPMA"])
+    extractionpercent
+
+    datamerge['Ext%'] = extractionpercent
+    datamerge['AqS#'] = datasplit_aqueous['S#']
+    datamerge['OrgS#'] = datasplit_organic['S#']
+    datamerge['Count Time'] = datasplit_organic['Count Time']
+    datamerge['DATE'] = datasplit_aqueous.DATE
+    datamerge['Isotope'] = datasplit_aqueous.Isotope
+    datamerge['AqL'] = datasplit_aqueous.Aq_Ligand
+    datamerge['AqLConc (mM)'] = datasplit_aqueous['Aq_Conc (mM)']
+    datamerge['OrgL'] = datasplit_organic.Org_Ligand
+    datamerge['OrgLConc (M)'] = datasplit_organic['Org_Conc (M)']
+    #
+    #
+    # # ### Time to plot this
+    #
+    date = datetime.datetime.strptime(datamerge.loc[0,'DATE'],'%m/%d/%Y').strftime('%Y%m%d')
+    #
+    print(datamerge)
+    #
+
+    # plt.plot(aqueous_concentrations,extractionpercent,'bo')
+    # plt.xscale('log')
+    # plt.title('HDEHP Extracts Gd Down to 2.5 mM')
+    # plt.xlabel('HDEHP Concentration (M)')
+    # plt.ylabel('% Extraction')
+    # plt.grid(True)
+    # plt.savefig(date+isotope+'_'+aqueous_ligand+'_'+organic_ligand+'.png')
+    # plt.show()
+    #export the data file as a CSV
+    datamerge.to_csv(date+isotope+'_'+aqueous_ligand+'_'+organic_ligand+'.csv')
