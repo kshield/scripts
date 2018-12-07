835f3ec7650b329486a085fc88cd950428d8e956
kshield <kshield@berkeley.edu>
2018-12-07 15:13:50 -0800

    Includes different pH values; changed marker shape by isotope

--- python/extractiongraphing.py
+++ python/extractiongraphing.py
@@ -68,28 +68,61 @@ else:
                 ligand_colors.append('rgba(60,180,75,')
 
 # Which isotopes are we graphing today?
-potential_isotopes = ['Ac227','Gd153','Lu177']
+potential_isotopes = ['Ac227','Gd153','Lu177','Ce134']
 # Isotope line format list
 Ac227line = 'dot'
 Gd153line = ''
 Lu177line = 'dash'
+Ce134line = 'longdash'
 allisotopes = input ('Do you want to graph all possible isotopes? (y/n): ')
 if allisotopes == 'y':
     graphed_isotopes = potential_isotopes
     isotope_lines = [Ac227line, Gd153line, Lu177line]
+    isotope_markers = ['circle','square','diamond','triangle']
 else:
     graphed_isotopes = []
     isotope_lines = []
+    isotope_markers = []
     for isotope in potential_isotopes:
         isotope_yes = input ('Do you want to graph '+isotope+'? (y/n): ')
         if isotope_yes == 'y':
             graphed_isotopes.append(isotope)
             if isotope == 'Ac227':
                 isotope_lines.append('dot')
+                isotope_markers.append('circle')
             elif isotope == 'Gd153':
                 isotope_lines.append('')
+                isotope_markers.append('square')
             elif isotope == 'Lu177':
                 isotope_lines.append('dash')
+                isotope_markers.append('diamond')
+            elif isotope == 'Ce134':
+                isotope_lines.append('longdash')
+                isotope_markers.append('triangle')
+
+# At what pH values are we plotting today?
+potential_pH = ['6','7','7.4']
+pH6marker = 'circle'
+pH7marker = 'square'
+pH7_4marker = 'diamond'
+desiredpH = input ('Do you want to graph all possible pH values? (y/n): ')
+if desiredpH == 'y':
+    graphed_pH = float(potential_pH)
+    pH_markers = [pH6marker, pH7marker, pH7_4marker]
+else:
+    graphed_pH = []
+    pH_markers = []
+    for pHstring in potential_pH:
+        pH_yes = input ('Do you want to graph '+pHstring+'? (y/n): ')
+        pH = float(pHstring)
+        if pH_yes == 'y':
+            graphed_pH.append(pH)
+            if pH == 6:
+                pH_markers.append('circle')
+            elif pH == 7:
+                pH_markers.append('square')
+            elif pH == 7.4:
+                pH_markers.append('diamond')
 
 # Warning about data
 print ('This script only incorporates pH 6 data; if you want other pH values, go elsewhere.')
@@ -103,149 +136,154 @@ graphdata = []
 for isotope in graphed_isotopes:
     isotopeindex = graphed_isotopes.index(isotope)
     linetype = isotope_lines[isotopeindex]
+    markershape = isotope_markers[isotopeindex]
     for ligand in graphed_ligands:
         ligandindex = graphed_ligands.index(ligand)
         linecolor = ligand_colors[ligandindex]
-        liganddata = datan[(datan['Ligand'] == ligand) &
-        (datan['Isotope'] == isotope) &
-        (datan['Initial pH'] == 6) &
-        (datan['Extractant Concentration (M)'] == 0.0025) &
-        #(datan['Date'] >= '10/10/2018') & (datan['Date'] <= '10/11/2018') &
-        (datan['Exclude Me'] != 'y')]
-        liganddata.sort_values('Ligand Concentration (mM)', inplace = True)
-        xvalues_all = liganddata['Ligand Concentration (mM)'].unique().tolist()
-        xvalues = []
-        yvalues = []
-        yerror = []
-        # figuring out how many data points are in triplicate
-        numberofentries = liganddata.groupby('Ligand Concentration (mM)').size().tolist()
-        if triplicateorno == 'average':
-            triplicatedataexists = []
-            for point in range(0,len(xvalues_all)):
-            #if the data is in triplicate or more:
-                if numberofentries[point] >= 3:
-                    value = liganddata.loc[(liganddata['Ligand Concentration (mM)'] == xvalues_all[point]), 'Extraction %'].mean()
-                    standard_deviation = liganddata.loc[(liganddata['Ligand Concentration (mM)'] == xvalues_all[point]), 'Extraction %'].std()
-                    xvalues.append(xvalues_all[point])
-                    yvalues.append(value)
-                    yerror.append(standard_deviation)
-                    triplicatedataexists.append(point)
-                elif numberofentries[point] <= 2 and isotope == 'Ac227':
-                    maxvalue = liganddata.iloc[0]['Extraction %']
-                    value = liganddata.loc[(liganddata['Ligand Concentration (mM)'] == xvalues_all[point]), 'Extraction %'].mean()
-                    normalizedvalue = value/maxvalue
-                    standard_deviation = 0
-                    xvalues.append(xvalues_all[point])
-                    yvalues.append(normalizedvalue)
-                    yerror.append(standard_deviation)
-                    triplicatedataexists.append(point)
-            if len(triplicatedataexists) >= 1:
-                trace = go.Scatter(
-                    x = xvalues,
-                    y = yvalues,
-                    mode = 'markers+lines',
-                    name = ligand+'-'+isotope,
-                    line = dict(color = linecolor+'1)', dash = linetype),
-                    error_y = dict(
-                        type = 'data',
-                        array = yerror,
-                        visible = 'True',
-                        color = linecolor+'.5)'
+        for pH in graphed_pH:
+            liganddata = datan[(datan['Ligand'] == ligand) &
+            (datan['Isotope'] == isotope) &
+            (datan['Initial pH'] == pH) &
+            (datan['Extractant Concentration (M)'] == 0.0025) &
+            #(datan['Date'] >= '10/10/2018') & (datan['Date'] <= '10/11/2018') &
+            (datan['Exclude Me'] != 'y')]
+            liganddata.sort_values('Ligand Concentration (mM)', inplace = True)
+            xvalues_all = liganddata['Ligand Concentration (mM)'].unique().tolist()
+            xvalues = []
+            yvalues = []
+            yerror = []
+            # figuring out how many data points are in triplicate
+            numberofentries = liganddata.groupby('Ligand Concentration (mM)').size().tolist()
+            if triplicateorno == 'average':
+                triplicatedataexists = []
+                for point in range(0,len(xvalues_all)):
+                #if the data is in triplicate or more:
+                    if numberofentries[point] >= 3:
+                        value = liganddata.loc[(liganddata['Ligand Concentration (mM)'] == xvalues_all[point]), 'Extraction %'].mean()
+                        standard_deviation = liganddata.loc[(liganddata['Ligand Concentration (mM)'] == xvalues_all[point]), 'Extraction %'].std()
+                        xvalues.append(xvalues_all[point])
+                        yvalues.append(value)
+                        yerror.append(standard_deviation)
+                        triplicatedataexists.append(point)
+                    elif numberofentries[point] <= 2 and isotope == 'Ac227':
+                        maxvalue = liganddata.iloc[0]['Extraction %']
+                        value = liganddata.loc[(liganddata['Ligand Concentration (mM)'] == xvalues_all[point]), 'Extraction %'].mean()
+                        normalizedvalue = value/maxvalue
+                        standard_deviation = 0
+                        xvalues.append(xvalues_all[point])
+                        yvalues.append(normalizedvalue)
+                        yerror.append(standard_deviation)
+                        triplicatedataexists.append(point)
+                if len(triplicatedataexists) >= 1:
+                    trace = go.Scatter(
+                        x = xvalues,
+                        y = yvalues,
+                        mode = 'markers+lines',
+                        name = ligand+'-'+isotope,
+                        line = dict(color = linecolor+'1)', dash = linetype),
+                        marker = dict(color = linecolor+'1)', symbol = markershape, size = 10),
+                        error_y = dict(
+                            type = 'data',
+                            array = yerror,
+                            visible = 'True',
+                            color = linecolor+'.5)'
+                            )
                         )
-                    )
-                graphdata.append(trace)
-            else:
-                xvalues = xvalues_all
-                yvalues = liganddata['Extraction %']
-                trace = go.Scatter(
-                    x = xvalues,
-                    y = yvalues,
-                    mode = 'markers+lines',
-                    name = ligand+'-'+isotope,
-                    line = dict(color = linecolor+'1)', dash = linetype)
-                    )
-                graphdata.append(trace)
-        if triplicateorno == 'individual':
-            datecolors = input ('Should individual dates get different colors? (y/n): ')
-            if datecolors == 'n':
-                xvalues = liganddata['Ligand Concentration (mM)'].tolist()
-                yvalues = liganddata['Extraction %'].tolist()
-                trace = go.Scatter(
-                    x = xvalues,
-                    y = yvalues,
-                    mode = 'markers',
-                    name = ligand+'-'+isotope,
-                    line = dict(color = linecolor+'1)', dash = linetype)
-                    )
-                graphdata.append(trace)
-            elif datecolors == 'y':
-                dates_all = liganddata['Date'].unique().tolist()
-                #print (dates_all)
-                for date in dates_all:
-                    datespecificdata = liganddata[(liganddata['Date'] == date)]
-                    xvalues = datespecificdata['Ligand Concentration (mM)'].tolist()
-                    yvalues = datespecificdata['Extraction %'].tolist()
+                    graphdata.append(trace)
+                else:
+                    xvalues = xvalues_all
+                    yvalues = liganddata['Extraction %']
                     trace = go.Scatter(
                         x = xvalues,
                         y = yvalues,
                         mode = 'markers',
-                        name = ligand+'-'+isotope+': '+ date,
-                        line = dict(dash = linetype)
+                        name = ligand+'-'+isotope,
+                        line = dict(color = linecolor+'1)', dash = linetype),
+                        marker = dict(color = linecolor+'1)', symbol = markershape, size = 10),
                         )
                     graphdata.append(trace)
-        # if no data exists in triplicate, plot the single data that exists with no error bars
+            if triplicateorno == 'individual':
+                datecolors = input ('Should individual dates get different colors? (y/n): ')
+                if datecolors == 'n':
+                    xvalues = liganddata['Ligand Concentration (mM)'].tolist()
+                    yvalues = liganddata['Extraction %'].tolist()
+                    trace = go.Scatter(
+                        x = xvalues,
+                        y = yvalues,
+                        mode = 'markers',
+                        name = ligand+'-'+isotope,
+                        line = dict(color = linecolor+'1)', dash = linetype),
+                        marker = dict(color = linecolor+'1)', symbol = markershape, size = 10),
+                        )
+                    graphdata.append(trace)
+                elif datecolors == 'y':
+                    dates_all = liganddata['Date'].unique().tolist()
+                    #print (dates_all)
+                    for date in dates_all:
+                        datespecificdata = liganddata[(liganddata['Date'] == date)]
+                        xvalues = datespecificdata['Ligand Concentration (mM)'].tolist()
+                        yvalues = datespecificdata['Extraction %'].tolist()
+                        trace = go.Scatter(
+                            x = xvalues,
+                            y = yvalues,
+                            mode = 'markers',
+                            name = ligand+'-'+isotope+': '+ date,
+                            line = dict(dash = linetype)
+                            )
+                        graphdata.append(trace)
+            # if no data exists in triplicate, plot the single data that exists with no error bars
 
-        layout = go.Layout(
-            title = graphtitle,
-                titlefont = dict(
-                    family = 'Georgia',
-                    size = 30
-                ),
-            xaxis = dict(
-                type = 'log',
-                #autorange = True,
-                domain = [0,1],
-                range = [-5.2,3],
-                title = 'Ligand Concentration (mM)',
-                titlefont = dict(
-                    family = 'Georgia',
-                    size = 22
-                ),
-                tickfont = dict(
-                    family = 'Georgia',
-                    size = 22
-                ),
-                showgrid = True,
-                showline = True,
-                exponentformat = 'e',
-                tick0 = -4,
-                dtick = 1),
-            yaxis = dict(
-                type = 'linear',
-                autorange = False,
-                domain = [0,1],
-                range = [0,1.1],
-                title = 'Relative Extraction',
-                titlefont = dict(
-                    family = 'Georgia',
-                    size = 22
-                ),
-                tickfont = dict(
-                    family = 'Georgia',
-                    size = 22
-                ),
-                showgrid = True,
-                showline = True),
-            legend = dict(
-                font = dict(
-                    family = 'Georgia',
-                    size = 14
-                ),
-                orientation = 'h',
-                xanchor = 'center',
-                x = 0.5,
-                y = 1.05
+            layout = go.Layout(
+                title = graphtitle,
+                    titlefont = dict(
+                        family = 'Georgia',
+                        size = 30
+                    ),
+                xaxis = dict(
+                    type = 'log',
+                    #autorange = True,
+                    domain = [0,1],
+                    range = [-5.2,3],
+                    title = 'Ligand Concentration (mM)',
+                    titlefont = dict(
+                        family = 'Georgia',
+                        size = 22
+                    ),
+                    tickfont = dict(
+                        family = 'Georgia',
+                        size = 22
+                    ),
+                    showgrid = True,
+                    showline = True,
+                    exponentformat = 'e',
+                    tick0 = -4,
+                    dtick = 1),
+                yaxis = dict(
+                    type = 'linear',
+                    autorange = False,
+                    domain = [0,1],
+                    range = [0,1.1],
+                    title = 'Relative Extraction',
+                    titlefont = dict(
+                        family = 'Georgia',
+                        size = 22
+                    ),
+                    tickfont = dict(
+                        family = 'Georgia',
+                        size = 22
+                    ),
+                    showgrid = True,
+                    showline = True),
+                legend = dict(
+                    font = dict(
+                        family = 'Georgia',
+                        size = 14
+                    ),
+                    orientation = 'h',
+                    xanchor = 'center',
+                    x = 0.5,
+                    y = 1.05
+                )
             )
-        )
     fig = go.Figure(data=graphdata, layout=layout)
 plot = py.offline.plot(fig, image = 'svg')
